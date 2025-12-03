ead740e29ddc4c7f607b908d0835648f3df57113
Bashar Galal Alhimyari <basharalhimyari77@gmail.com>
2025-12-03 10:52:14 -0800

    modify and add jwt to login function

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,9 @@
 const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const User = require("../models/user");
+const Token = require("../models/token");
+const generateJWT = require('../utils/generateJWT')
+
 
 exports.register = async (req, res) => {
     const error = validationResult(req);
@@ -22,14 +25,13 @@ exports.register = async (req, res) => {
         return res.status(201).json({ message: result });
     } catch (error) {
         if (error.message.includes("duplicate key error collection")) {
-            return res.status(409).json({ message: 'User already exists' });
+            return res.status(409).json({ type: "AuthError", message: 'User is already exists' });
         }
         return res.status(500).json({ message: error.message, type: "Internal Server Error" });
     }
 }
 
 exports.login = async (req, res) => {
-
     try {
         let { user_email, user_password } = req.body
         if (!user_email || !user_password) {
@@ -37,16 +39,28 @@ exports.login = async (req, res) => {
         }
 
         const user = await User.findOne({ user_email: user_email })
-        
+
         if (!user) {
             return res.status(404).send({ status: "FAIL", data: { user: "user not found" } })
         }
 
         const matchPassword = await bcrypt.compare(user_password, user.user_password)
-        
-         if (user && matchPassword) {
 
-            return res.status(200).send({ message :" success" })
+        if (user && matchPassword) {
+            const accessToken = await generateJWT({ user_email: user.user_email, id: user._id, isAdmin: user.is_admin })
+            const refreshToken = await generateJWT(
+                { user_email: user.user_email, id: user._id, isAdmin: user.is_admin },
+                process.env.REFRESH_JWT_SECRET,
+                "7d")
+
+            const token = await Token.findOne({ userId: user._id })
+            if (token) {
+                await token.deleteOne()
+            }
+            await new Token({ userId: user._id, accessToken, refreshToken }).save()
+
+            user.user_password = undefined // to hide the password from the response
+            return res.status(200).send({ message: " success", ...user._doc, accessToken })
         } else {
             return res.status(400).send({ status: "FAIL", message: "Invalid credentials" })
         }
@@ -79,6 +93,7 @@ exports.forgetPassword = async (req, res) => {
 
 exports.verifyOtp = async (req, res) => {
 }
+
 
 exports.resetPassword = async (req, res) => {
 }
